122460ca073a0070cf5ca83d0c7ad6394ee34911
dmitrymitrakhovich <dmitrymitrakhovich@gmail.com>
2023-04-21 16:36:54 +0400

    set pwa settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
+
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  outDir: 'dist',
+  manifest: {
+    name: 'Dev finder',
+    short_name: 'dev_finder',
+    description: '',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'assets/images/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/images/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), vitePWA],
   resolve: {
     alias: {
       components: '/src/components',

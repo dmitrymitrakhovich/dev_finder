2a34973dfe87a55149d6adec381c746fda48579c
dmitrymitrakhovich <dmitrymitrakhovich@gmail.com>
2023-04-21 17:36:55 +0400

    update base in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ const vitePWA = VitePWA({
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'dev_finder',
   plugins: [react(), svgr(), vitePWA],
   resolve: {
     alias: {

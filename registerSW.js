ee14b29b5b1ada49fcd1a25eba63fc20dcb7f3c2
dmitrymitrakhovich <dmitrymitrakhovich@gmail.com>
2023-04-21 17:51:58 +0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dev_finder/sw.js', { scope: '/dev_finder/' })})}
